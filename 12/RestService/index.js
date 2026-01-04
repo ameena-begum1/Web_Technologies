3f2d4d0595da5b03ca65c7de1100e9e36d46f756
ameena-begum1 <ameenahsyed2003@gmail.com>
2026-01-04 20:44:55 +0530

    final

--- 12/RestService/index.js
+++ 12/RestService/index.js
@@ -1,28 +1,36 @@
-var express = require('express');
-var cors = require('cors');
+//import express, cors, file system(fs) to read json files
+var express = require("express");
+var cors = require("cors");
 var fs = require("fs");
 
+//create express app
 var app = express();
-app.use(cors());
+app.use(cors()); // any client can call API
 
-app.get('/students', function(req, res) {
-  fs.readFile(__dirname + "/students.json", 'utf8', function(err, data) {
-    res.end(data);
-  });
+//create APIs
+app.get("/students", function(req, res) {
+    //readfile
+    fs.readFile(__dirname+ "/students.json", "utf8", function(err, data) {
+        if(err) throw err;
+        res.end(data);
+    });
 });
 
-app.get('/imarks', function(req, res) {
-  fs.readFile(__dirname + "/imarks.json", 'utf8', function(err, data) {
-    res.end(data);
-  });
+app.get("/imarks", function(req, res) {
+    fs.readFile(__dirname+ "/imarks.json", "utf8", function(err, data) {
+        if(err) throw err;
+        res.end(data);
+    });
 });
 
-app.get('/emarks', function(req, res) {
-  fs.readFile(__dirname + "/emarks.json", 'utf8', function(err, data) {
-    res.end(data);
-  });
+app.get("/emarks", function(req, res) {
+    fs.readFile(__dirname+ "/emarks.json", "utf8", function(err, data) {
+        if(err) throw err;
+        res.end(data);
+    });
 });
 
-app.listen(5000, () => {
-  console.log("REST Service running on port 8080");
+//start the server 
+app.listen(8080, function() {
+    console.log("Server started at port: 8080");
 });
--- 12/spa/src/App.js
+++ 12/spa/src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
@@ -13,10 +12,6 @@ class App extends Component {
   }
 
   componentDidMount() {
-    this.loadStudents();
-  }
-
-  loadStudents() {
     fetch("http://localhost:8080/students")
       .then(res => res.json())
       .then(result => {
@@ -24,21 +19,21 @@ class App extends Component {
       });
   }
 
-  showInternalMarks() {
+  showInternalMarks = () => {
     fetch("http://localhost:8080/imarks")
       .then(res => res.json())
       .then(result => {
         this.setState({ internalMarks: result });
       });
-  }
+  };
 
-  showExternalMarks() {
+  showExternalMarks = () => {
     fetch("http://localhost:8080/emarks")
       .then(res => res.json())
       .then(result => {
         this.setState({ externalMarks: result });
       });
-  }
+  };
 
   render() {
     const { studentData, internalMarks, externalMarks } = this.state;
@@ -47,40 +42,46 @@ class App extends Component {
       <div className="App">
         <h1>Student Marks Information</h1>
 
-        <button onClick={() => this.showInternalMarks()}>
+        <button onClick={this.showInternalMarks}>
           Show Internal Marks
         </button>
 
-        <button onClick={() => this.showExternalMarks()}>
+        <button onClick={this.showExternalMarks}>
           Show External Marks
         </button>
 
+        <br /><br />
+
         <table border="1" align="center">
-          <tr>
-            <th>Student Name</th>
-            <th>Internal</th>
-            <th>External</th>
-          </tr>
+          <thead>
+            <tr>
+              <th>Student Name</th>
+              <th>Internal Marks</th>
+              <th>External Marks</th>
+            </tr>
+          </thead>
 
-          <tr>
-            <td>
-              {studentData.map(s => (
-                <div key={s.id}>{s.name}</div>
-              ))}
-            </td>
+          <tbody>
+            <tr>
+              <td>
+                {studentData.map(s => (
+                  <div key={s.id}>{s.name}</div>
+                ))}
+              </td>
 
-            <td>
-              {internalMarks.map(i => (
-                <div key={i.id}>{i.marks}</div>
-              ))}
-            </td>
+              <td>
+                {internalMarks.map(i => (
+                  <div key={i.id}>{i.marks}</div>
+                ))}
+              </td>
 
-            <td>
-              {externalMarks.map(e => (
-                <div key={e.id}>{e.marks}</div>
-              ))}
-            </td>
-          </tr>
+              <td>
+                {externalMarks.map(e => (
+                  <div key={e.id}>{e.marks}</div>
+                ))}
+              </td>
+            </tr>
+          </tbody>
         </table>
       </div>
     );
